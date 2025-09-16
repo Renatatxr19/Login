e14b76a1c2f41d8b2b670387e6a56b992ce12232
Renata Teixeira de Jesus <renata.teixeira@dinfor.uniceub>
2025-09-16 09:36:40 -0300

    primeiro commit

---
+++ aprendendo.botao/script.js
@@ -0,0 +1,8 @@
+document.getElementById("botao_1").onclick = function() 
+{alert("Você clicou no botão A");};
+
+document.getElementById("botao_2").onclick = function() 
+{alert("Você clicou no botão B");};
+
+document.getElementById("botao_3").onclick = function() 
+{alert("Você clicou no botão C");};
